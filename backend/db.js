bfe46a1f5e64783748ab029213e7792a8b5fe7cd
sangwon <lee5507@soongsil.ac.kr>
2020-11-10 18:56:36 +0900

    travis ci deploy added

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
     connectionLimit:10,
-    hoat:'mysql',
-    user:'root',
-    password:'sangwon',
-    database:'myapp'
+    hoat: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 });
 
 exports.pool = pool;
